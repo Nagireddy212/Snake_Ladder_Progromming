9448b7412131299f3076789495131b236ee1ac99
unknown <somulanagireddy123@gmail.com>
2021-03-11 07:33:25 +0530

    [Nagireddy]: added .gitignore file

---
+++ Snake_Ladder.java
@@ -0,0 +1,10 @@
+public class Snake_Ladder {
+    public static final int startPosition = 0;
+    public static final int winningPosition = 100;
+    public static void main(String[] args) 
+    {
+       System.out.println("Welcome To Snake And Ladder Game");
+       System.out.println("Start Position is: "  + startPosition);
+       System.out.println("Winning Position is: "  + winningPosition);
+     }
+ }
